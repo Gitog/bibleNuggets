f679af73467201520e1d3ff52112e5ff4be40f87
James <jamesnjorio@gmail.com>
2022-09-02 10:41:06 +0300

    added a fetch function to js file

--- js/nugget.js
+++ js/nugget.js
@@ -1,2 +1,17 @@
 // Testing js connection
-console.log("Connected....")
+console.log("Connected....");
+//selecting html elements
+let searchBar = document.querySelector('input');
+let searchButton = document.querySelector('button');
+//Log elements we have selected to confirm they are working
+console.log(searchBar);
+console.log(searchButton);
+
+function getVerse() {
+    fetch('https://bible-api.com/jn 3:16')
+        .then(verses => verses.json())
+        .then(reference => console.log(reference))
+}
+
+//call getverse to test if its fetching correctly
+getVerse()
